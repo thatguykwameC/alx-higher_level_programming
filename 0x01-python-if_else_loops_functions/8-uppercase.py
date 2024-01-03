447296c94df7481f78c1e72e7418a0278ee5f5a6
Cleveland Ataafi <124477199+thatguykwameC@users.noreply.github.com>
2024-01-03 01:32:18 -0500

    Update to Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def uppercase(char):
-    for i char:
+    for i in char:
         if ord(i) >= 97 and ord(i) <= 122:
             i = chr(ord(i) - 32)
         print("{}".format(i), end="")
