02b10c19995cad114806d0874fa1e0766bf0c26b
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-01-04 10:37:14 -0800

    Solution for Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/nin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    counter = len(sys.argv) - 1
+
+    if counter == 0:
+        print("0 arguements.")
+    elif counter == 1:
+        print("1 arguements:")
+    else:
+        print("{} arguements:".format(counter))
+    for i in range(counter):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
