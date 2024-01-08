2b39fe4878f68127f5b91b4b92bc2f37d8296d66
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-01-08 01:49:42 -0800

    Changed commenting style

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    """
-    my_list: The list of integers to be printed
-    """
-
+    """my_list: The list of integers to be printed"""
     for integer in my_list:
         print("{:d}".format(integer))
