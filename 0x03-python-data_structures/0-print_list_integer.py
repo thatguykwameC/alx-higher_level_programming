89ebf35c5736995ec9254a38d319563f78037b3c
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-01-08 01:17:41 -0800

    A function that prints int from list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    my_list: The list of integers to be printed
+    """
+
+    for integer in my_list:
+        print("{:d}".format(integer))
