a1e921ec40a7eab47a7f2e2885fe66885033d548
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-01-10 08:37:04 -0800

    Edits to function

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -2,4 +2,4 @@
 
 def square_matrix_map(matrix=[]):
     """Returns the square values of all integers in a matrix"""
-    return list(map((lambda row: list(map((lambda x: x * x), row))), matrix))
+    return list(map((lambda row: list(map((lambda x: x**2), row))), matrix))
