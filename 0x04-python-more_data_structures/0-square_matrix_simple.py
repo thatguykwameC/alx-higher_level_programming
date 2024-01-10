03a691744e88a3505555f5f4603248d78e43be7e
Cleveland Ataafi <124477199+thatguykwameC@users.noreply.github.com>
2024-01-10 11:48:34 -0500

    Removed comments

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    """This function returns a matrix with it's elements squared"""
     return[list(map(lambda element: element**2, row)) for row in matrix]
