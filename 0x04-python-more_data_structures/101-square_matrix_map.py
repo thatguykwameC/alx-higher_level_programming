bd84d206bd6bc39ce224e461e718c2a1c30369bc
Cleveland Ataafi <124477199+thatguykwameC@users.noreply.github.com>
2024-01-10 11:45:11 -0500

    Update pyhton style

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
     """Returns the square values of all integers in a matrix"""
     return list(map((lambda row: list(map((lambda x: x**2), row))), matrix))
