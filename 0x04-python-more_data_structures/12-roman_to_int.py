24e2328c8078a30b7c064f946f7a30d469abc7a2
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-01-10 09:24:02 -0800

    Refactored function

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
-def roman_to_int(roman_string):
+def roman_to_int(roman_string: str) -> int:
     """Converts roman numerals to integers"""
-    if (not isinstance(roman_string), str or
-            roman_string is None):
-        return 0
-
-    numerals = {
+    rom_numerals = {
         "I": 1,
         "V": 5,
         "X": 10,
@@ -15,14 +11,17 @@ def roman_to_int(roman_string):
         "D": 500,
         "M": 1000,
     }
-    num = 0
 
-    for i in range(len(roman_string)):
-        if numerals.get(roman_string[i], 0) == 0:
-            return 0
-        if (i != len(roman_string) - 1) and
-            numerals[roman_string[i]] < numerals[roman_string[i + 1]]):
-            num += numerals[roman_string[i]] * -1
-        else:
-            num += numerals[roman_string[i]]
-    return num
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    return sum(
+        rom_numerals.get(roman_string[i], 0)
+        if i == 0
+        or rom_numerals.get(roman_string[i], 0)
+        <= rom_numerals[roman_string[i - 1]]
+
+        else rom_numerals.get(roman_string[i], 0)
+        - 2 * rom_numerals.get(roman_string[i - 1], 0)
+        for i in range(len(roman_string))
+    )
