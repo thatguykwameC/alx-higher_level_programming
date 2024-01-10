0608d5f73233b20678036cab001ca6c74118ffb8
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-01-10 07:50:44 -0800

    A script that returns the square values of integers in a matrix

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def square_matrix_map(matrix=[]):
+    """Returns the square values of all integers in a matrix"""
+    return list(map((lambda row: list(map((lambda x: x * x), row))), matrix))
