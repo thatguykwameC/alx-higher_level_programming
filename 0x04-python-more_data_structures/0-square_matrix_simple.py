ffc57eeff28b77807fafb63308da5edd66dd014b
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-01-10 06:31:37 -0800

    A function that squares elements of a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    """This function returns a matrix with it's elements squared"""
+    return[list(map(lambda element: element**2, row)) for row in matrix]
