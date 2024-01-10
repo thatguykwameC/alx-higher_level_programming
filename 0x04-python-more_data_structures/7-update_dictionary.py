4b35ee4dd29da05645010a18e15091738c2979dd
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-01-10 07:09:57 -0800

    A script that uppdates a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary: dict, key: str, value) -> dict:
+    """Updates values in a dictionary"""
+    a_dictionary[key] = value
+
+    return a_dictionary
