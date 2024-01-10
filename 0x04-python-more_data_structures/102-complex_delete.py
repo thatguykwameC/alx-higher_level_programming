54c44dfa281e267bfd72c422a957f352f91dcf61
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-01-10 07:57:30 -0800

    Deletes keys with a specific value

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    """Deletes keys with a specified value in a dictionary"""
+    keys_to_del = []
+    for key in a_dictionary:
+        if a_dictionary[key] == value:
+            keys_to_del.append(key)
+
+    for key in keys_to_del:
+        del a_dictionary[key]
+    return a_dictionary
