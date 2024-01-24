a36e8695644675cf95ae01c6022e70d12a76de0e
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-01-23 18:01:11 -0800

    A class square defined by other class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a blueprint for class square"""
+
+
+class Square:
+    """Represents the class square with a private instance"""
+
+    def __init__(self, size):
+        """Initalizes the class square"""
+        self.__size = size
