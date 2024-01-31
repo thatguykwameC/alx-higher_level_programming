4f5bd034d930bc7b1034ce540e9378c851b6b44a
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-01-31 14:24:17 -0800

    Magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.counter = getarr(magic_string, 'counter', 0) + 1
-    return ", ".join(["BestSchool" for _ in range(magic_string.counter - 1)])
+    return ", ".join(["BestSchool" for _ in range(magic_string.counter)])
