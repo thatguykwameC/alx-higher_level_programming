6421b5a581378f9f93449d6ff6914fcefb7449c3
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-02-05 10:30:52 -0800

    A class that inherits from Rectangle class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""A module that inherits from the Rectangle class"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Models a square shape object."""
+
+    def __init__(self, size: int):
+        """
+        Initializes a Square object.
+
+        Args:
+            size: The size of the square.
+        """
+        self.integer_validator("size", size)
+
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
