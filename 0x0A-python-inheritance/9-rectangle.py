dbbcd97a122b2228550753fbee8a87236179cdb1
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-02-05 10:20:45 -0800

    A rectangle that inherits from BaseGeometry

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    This class represents a rectangle using BaseGeometry
+    Args:
+        width: The width of the rectangle
+        height: The height of the rectangle
+    """
+
+    def __init__(self, width, height):
+        """Intialize a new rectangle"""
+
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """returns the print() and str() representation of a Rectangle"""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
