0ef0b5c4ac9d44915b8265841bfd709014990bd6
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-02-05 09:36:04 -0800

    A function that checks if an obj is the same instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+A module that returns True if the object is exactly an
+instance of the specified class.
+"""
+
+
+def is_same_class(obj: object, a_class):
+    """
+    Returns True if `obj` is an instance of `a_class`.
+
+    Args:
+        obj (object): The object to check.
+        a_class (class): The class to check if obj  is an instance
+    """
+    return type(obj) is a_class
