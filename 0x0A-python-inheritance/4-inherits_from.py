d4ff4fa84d09fbf9dd58df2afc2eecb2cf2a05be
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-02-05 09:54:04 -0800

    A module that returns if the object is an instance of a class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A module that returns True if the object is an instance of a class that inherited
+(directly or indirectly) from the specified class, else False
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False.
+
+    Args:
+        obj (object): The object to check.
+        a_class (class): The class to check if `obj` is an instance of.
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
