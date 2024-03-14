dbedb43b8c7c02942d785813e838071b9f8ed7bf
Cleveland Ataafi <thatguykwamec@gmail.com>
2024-03-13 22:44:48 -0700

    More queries

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- This script create the table 'force_name'
+
+CREATE TABLE force_name(
+	id INT,
+	name VARCHAR(256)
+);
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- This script creates the database 'hbtn_0d_usa' and the table 'city'
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT fk_cities_state_id FOREIGN KEY (state_id)
+	REFERENCES states (id)
+);
