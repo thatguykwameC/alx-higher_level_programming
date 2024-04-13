52db21dd3dc019be4047bcbd5bf0f2a9bc60f34f
thatguykwameC <thatguykwamec@gmail.com>
2024-04-13 08:29:40 -0500

    Filters rows and matches to provided arguements

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""This script displays all rows from the `states` table in the database
+`hbtn_0e_0_usa` where the `name` column matches the provided argument."""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    # Retrieving command-line arguments for database connection and state name
+    db_username, db_password, db_name, state_name = sys.argv[1:]
+
+    # Establishing connection to the MySQL database
+    connection = MySQLdb.connect(
+        host="localhost",
+        user=db_username,
+        password=db_password,
+        database=db_name,
+        port=3306,
+    )
+
+    # Creating a cursor object to execute SQL queries
+    db_cursor = connection.cursor()
+
+    query = """
+        SELECT * FROM states
+        WHERE name LIKE BINARY %s
+        ORDER BY states.id;
+    """
+    db_cursor.execute(query, (state_name,))
+
+    # Fetching all rows from the result set
+    result_set = db_cursor.fetchall()
+
+    # Displaying each row in the result set
+    for state_row in result_set:
+        print(state_row)
+
+    # Closing the cursor and database connection
+    db_cursor.close()
+    connection.close()
