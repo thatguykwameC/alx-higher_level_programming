cdf8c206ac130009cde9cc4e48c72f6faaec09cf
thatguykwameC <thatguykwamec@gmail.com>
2024-04-13 08:21:02 -0500

    filters all records starting with 'N'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,43 @@
 #!/usr/bin/python3
+
+"""This script retrieves and displays all states with names
+starting with 'N'from the database `hbtn_0e_0_usa`."""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    # Extracting database credentials from command-line arguments
+    db_username, db_password, db_name = sys.argv[1:]
+
+    # Establishing connection to the MySQL database
+    connection = MySQLdb.connect(
+        host="localhost",
+        user=db_username,
+        password=db_password,
+        database=db_name,
+        port=3306,
+    )
+
+    # Creating a cursor object to execute SQL queries
+    db_cursor = connection.cursor()
+
+    # Executing SQL query to retrieve states with names starting with 'N'
+    db_cursor.execute(
+        """
+        SELECT * FROM states
+        WHERE name LIKE BINARY 'N%'
+        ORDER BY states.id;
+        """
+    )
+
+    # Fetching all rows from the result set
+    result_set = db_cursor.fetchall()
+
+    # Displaying each row in the result set
+    for state_row in result_set:
+        print(state_row)
+
+    # Closing the cursor and database connection
+    db_cursor.close()
+    connection.close()
