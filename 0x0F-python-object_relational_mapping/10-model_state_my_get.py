7f4b2c15c26507a3ca27a9908f3e6ceb4431c4f1
thatguykwameC <thatguykwamec@gmail.com>
2024-04-15 00:20:39 -0500

    Prints state obj with its name passed as an arg

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""This module prints the State object with the name passed
+as an argument from the `hbtn_0e_6_usa` database"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+def print_state_by_name(
+    username: str, password: str, database: str, state_name: str
+):
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
+    )
+
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        state = session.query(State).filter(State.name == state_name).first()
+
+        if state:
+            print(f"{state.id}")
+        else:
+            print("Not found")
+
+
+if __name__ == "__main__":
+    try:
+        print_state_by_name(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    except IndexError:
+        sys.stderr.write(
+            f"Usage: {sys.argv[0]} <username> <password> "
+            "<database> <state name to search>\n"
+        )
+        sys.exit(1)
