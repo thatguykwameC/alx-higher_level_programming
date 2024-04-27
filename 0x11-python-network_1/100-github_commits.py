69645739f7b251f92b405d9f3a418923de009014
thatguykwameC <thatguykwamec@gmail.com>
2024-04-27 14:30:57 -0500

    Gets the last 10 commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest) of the repository and user
+sent in as arguments
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
+    response = requests.get(url)
+
+    commits = response.json()[:10]
+    for commit in commits:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
