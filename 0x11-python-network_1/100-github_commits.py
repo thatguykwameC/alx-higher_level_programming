be6a690939d133ee8ef3dce4420af9aedae6ad4e
thatguykwameC <thatguykwamec@gmail.com>
2024-04-27 14:34:34 -0500

    Gets the last 10 commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,7 +9,7 @@ from sys import argv
 
 if __name__ == '__main__':
 
-    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
+    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
     response = requests.get(url)
 
     commits = response.json()[:10]
