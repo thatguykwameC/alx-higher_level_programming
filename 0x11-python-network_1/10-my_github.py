2850f48b86850df6f105ebaa5022e829c1cb3339
thatguykwameC <thatguykwamec@gmail.com>
2024-04-27 13:23:36 -0500

    Uses GitHub API to display id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
 
     url = "https://api.github.com/user"
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    req = requests.get(url, auth=(user, passwd))
-    print(req.json().get('id'))
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get(url, auth=(username, password))
+    print(response.json().get('id'))
