376d9528aec07eebfcb2800c2300a68a9c299958
thatguykwameC <thatguykwamec@gmail.com>
2024-04-27 14:54:59 -0500

    Gets the last 10 commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,6 +10,10 @@ from sys import argv, exit
 
 if __name__ == '__main__':
 
+    if len(argv) < 3:
+        print(f"Usage: {argv[0]} <repo> <owner>")
+        exit(1)
+
     url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
     response = requests.get(url)
 
