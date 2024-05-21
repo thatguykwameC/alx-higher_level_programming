3de1937fde3a4ef5ecf27391273f7b7c6551457c
thatguykwameC <thatguykwamec@gmail.com>
2024-05-21 20:38:29 +0000

    Computes num of tasks by a user

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+// This script computes the number of tasks completed by user id.
+
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
+    });
+    console.log(completed);
+  }
+});
