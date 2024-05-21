7fa288eac9db4f027e2ed2186923e8d352d2dd64
thatguykwameC <thatguykwamec@gmail.com>
2024-05-21 20:44:29 +0000

    Prints all chars of a movie

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+// This script prints all characters of a Star Wars movie
+// Character names are displayed in the same order of the list “characters”
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, (error, response) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const characters = JSON.parse(response.body).characters;
+  const characterPromises = characters.map((character) => {
+    return new Promise((resolve, reject) => {
+      request(character, (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(JSON.parse(response.body).name);
+        }
+      });
+    });
+  });
+
+  Promise.all(characterPromises)
+    .then((names) => {
+      names.forEach((name) => console.log(name));
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
