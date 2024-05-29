503324ce097909b6111083a3f9639c6f0329e7d9
thatguykwameC <thatguykwamec@gmail.com>
2024-05-29 16:54:24 -0400

    fetches char from a URL

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,6 +1,3 @@
-// Fetches the StarWars character name from
-// 'https://swapi-api.alx-tools.com/api/people/5/?format=json'
-
 const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
 
 $.get(url, (data, status) => {
